06d8a049057c49ab688fecb17052498c835ae5eb
Usman Quddusi <usmanquddusi@gmail.com>
2018-09-16 23:02:19 +1000

    ADD” option on new stats page + •	Long press, row item will show to edit/delete options

---
+++ TimeTracker.Android/obj/Debug/android/src/md5f905ed2781c0bef5307b57bab067f6ad/SplashActivity.java
@@ -0,0 +1,47 @@
+package md5f905ed2781c0bef5307b57bab067f6ad;
+
+
+public class SplashActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("TimeTracker.Droid.SplashActivity, TimeTracker.Android", SplashActivity.class, __md_methods);
+	}
+
+
+	public SplashActivity ()
+	{
+		super ();
+		if (getClass () == SplashActivity.class)
+			mono.android.TypeManager.Activate ("TimeTracker.Droid.SplashActivity, TimeTracker.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
